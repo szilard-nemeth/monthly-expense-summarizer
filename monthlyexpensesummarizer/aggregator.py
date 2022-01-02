96441f04b8a7f59a37c6e19bfc3b568100180705
Szilard Nemeth <szilard.nemeth88@gmail.com>
2022-01-02 21:37:17 +0100

    aggregator.py: Save state to Aggregator object

--- monthlyexpensesummarizer/aggregator.py
+++ monthlyexpensesummarizer/aggregator.py
@@ -7,39 +7,39 @@ LOG = logging.getLogger(__name__)
 
 
 class Aggregator:
-    @classmethod
-    def aggregate(cls, parsed_expenses: List[ParsedExpense]):
-        by_payment_method: Dict[str, int] = {}
-        by_payment_method_stringified: Dict[str, str] = {}
-        by_day: Dict[str, int] = {}
-        by_transaction_type: Dict[ItemType, int] = {}
+    def __init__(self):
+        self.by_payment_method: Dict[str, int] = {}
+        self.by_payment_method_stringified: Dict[str, str] = {}
+        self.by_day: Dict[str, int] = {}
+        self.by_transaction_type: Dict[ItemType, int] = {}
+
+    def aggregate(self, parsed_expenses: List[ParsedExpense]):
         for expense in parsed_expenses:
             key = expense.date
-            if key not in by_day:
-                by_day[key] = 0
-            by_day[key] += expense.amount
+            if key not in self.by_day:
+                self.by_day[key] = 0
+            self.by_day[key] += expense.amount
 
             item_type = expense.item_type
             if item_type == ItemType.EXPENSE:
                 key = expense.payment_method.display_name
-                if key not in by_payment_method:
-                    by_payment_method[key] = 0
-                    by_payment_method_stringified[key] = ""
-                by_payment_method[key] += expense.amount
-                by_payment_method_stringified[key] += f"{expense.amount}+"
+                if key not in self.by_payment_method:
+                    self.by_payment_method[key] = 0
+                    self.by_payment_method_stringified[key] = ""
+                self.by_payment_method[key] += expense.amount
+                self.by_payment_method_stringified[key] += f"{expense.amount}+"
 
             key = item_type
-            if key not in by_transaction_type:
-                by_transaction_type[key] = 0
-            by_transaction_type[key] += expense.amount
+            if key not in self.by_transaction_type:
+                self.by_transaction_type[key] = 0
+            self.by_transaction_type[key] += expense.amount
 
         LOG.info("Listing aggregates...")
-        for payment_method, amount in by_payment_method.items():
+        for payment_method, amount in self.by_payment_method.items():
             LOG.info("Aggregate expenses for payment method '%s': %d", payment_method, amount)
             LOG.info("Stringified aggregate expenses for payment method '%s': %s", payment_method, by_payment_method_stringified[payment_method])
-        for day, amount in by_day.items():
+        for day, amount in self.by_day.items():
             LOG.info("Aggregate expenses for day '%s': %d", day, amount)
-        for tx_type, amount in by_transaction_type.items():
+        for tx_type, amount in self.by_transaction_type.items():
             LOG.info("Aggregate expenses by transaction type '%s': %d", tx_type, amount)
-
 
